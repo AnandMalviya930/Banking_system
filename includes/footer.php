017c658bf2c30af6154a5bb14d0e46afaff93bef
AnandMalviya930 <95901670+AnandMalviya930@users.noreply.github.com>
2022-01-08 15:57:20 +0530

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="container"> 
+        <center> 
+            <p>Copyright &copy;The sparks foundation project| Contact Anand Malviya: +91 93019 92508</p> 
+        </center>
+    </div> 
+</footer>
